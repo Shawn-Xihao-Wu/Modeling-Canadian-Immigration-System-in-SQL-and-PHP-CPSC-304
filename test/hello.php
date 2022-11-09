3038f2da582cfc6f330b8ec4d181ab96c6580bd8
UBC Student <shawnxhw@students.cs.ubc.ca>
2022-11-08 18:56:20 -0800

    Create a local test file that's not tracked by git using .gitignore

--- test/hello.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<p>If PHP is working, you will see "Hello World" below, followed by some PHP version information:</p><hr />
-	<?php
-		echo "Hello worl";
-		phpinfo();  // Print PHP version and config info
-	?>
-</html>
-
-
