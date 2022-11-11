60dbdb6b8b404a50e70b84dda26b5dbe057f884c
UBC Student <wkyi2021@gambier.students.cs.ubc.ca>
2022-11-10 21:40:01 -0800

    update for approved_institution

--- approved_institution.php
+++ approved_institution.php
@@ -128,7 +128,7 @@
 
             // Your username is ora_(CWL_ID) and the password is a(student number). For example,
 			// ora_platypus is the username and a12345678 is the password.
-            $db_conn = OCILogon("ora_hanzew25", "a39408695", "dbhost.students.cs.ubc.ca:1522/stu");
+            $db_conn = OCILogon("ora_wkyi2021", "a39028535", "dbhost.students.cs.ubc.ca:1522/stu");
 
             if ($db_conn) {
                 debugAlertMessage("Database is Connected");
